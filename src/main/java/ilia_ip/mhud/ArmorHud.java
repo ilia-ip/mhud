5dc1d9ab228790c771d6338448ed154834a37d05
ilia-ip <ip.infoboks@gmail.com>
2025-08-03 17:02:22 +0300

    initial commit

---
+++ src/main/java/ilia_ip/mhud/ArmorHud.java
@@ -0,0 +1,12 @@
+package ilia_ip.mhud;
+
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElement;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderTickCounter;
+
+public class ArmorHud implements HudElement {
+    @Override
+    public void render(DrawContext drawContext, RenderTickCounter renderTickCounter) {
+
+    }
+}
---
+++ src/main/java/ilia_ip/mhud/Mhud.java
@@ -0,0 +1,20 @@
+package ilia_ip.mhud;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElement;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Mhud implements ModInitializer {
+	public static final String MOD_ID = "mhud";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		HudElementRegistry.addFirst(Identifier.of(MOD_ID, "armor_hud"), new ArmorHud());
+	}
+}
