ac6c5cfce3df95b4bfc7f415f292eab1230b78f4
ilia-ip <ip.infoboks@gmail.com>
2025-08-04 14:59:50 +0300

    fixed config screen a little bit

--- src/main/java/ilia_ip/mhud/ConfigScreen.java
+++ src/main/java/ilia_ip/mhud/ConfigScreen.java
@@ -23,97 +23,110 @@ public class ConfigScreen extends Screen {
     protected void init() {
         int x = MinecraftClient.getInstance().getWindow().getScaledWidth() / 2 - 100;
 
-        TextWidget armor_text = new TextWidget(x, 80, 200, 20, Text.translatable("conf.mhud.armor_hud"), textRenderer);
-        ButtonWidget armor_hud = ButtonWidget.builder(
+        addDrawableChild(
+                new TextWidget(x, 80, 200, 20, Text.translatable("conf.mhud.armor_hud"), textRenderer)
+        );
+        addDrawableChild(
+                ButtonWidget.builder(
                 Mhud.CONFIG.ArmorHudEnabled ? ENABLED : DISABLED,
                 (widget) -> {
                     Mhud.CONFIG.ArmorHudEnabled = !Mhud.CONFIG.ArmorHudEnabled;
                     widget.setMessage(Mhud.CONFIG.ArmorHudEnabled ? ENABLED : DISABLED);
                     Mhud.CONFIG.save();
                 }
-        ).dimensions(x, 100, 200, 20).build();
+            ).dimensions(x, 100, 200, 20).build()
+        );
 
-        TextWidget potion_text = new TextWidget(x, 130, 200, 20, Text.translatable("conf.mhud.potion_hud"), textRenderer);
-        ButtonWidget potion_hud = ButtonWidget.builder(
+        addDrawableChild(
+                new TextWidget(x, 130, 200, 20, Text.translatable("conf.mhud.potion_hud"), textRenderer)
+        );
+        addDrawableChild(
+                ButtonWidget.builder(
                 Mhud.CONFIG.PotionHudEnabled ? ENABLED : DISABLED,
                 (widget) -> {
                     Mhud.CONFIG.PotionHudEnabled = !Mhud.CONFIG.PotionHudEnabled;
                     widget.setMessage(Mhud.CONFIG.PotionHudEnabled ? ENABLED : DISABLED);
                     Mhud.CONFIG.save();
                 }
-        ).dimensions(x, 150, 200, 20).build();
+            ).dimensions(x, 150, 200, 20).build()
+        );
 
-        TextWidget crosshair3d_text = new TextWidget(x, 180, 200, 20, Text.translatable("conf.mhud.crosshair3dp_hud"), textRenderer);
-        ButtonWidget crosshair3d_hud = ButtonWidget.builder(
+        addDrawableChild(
+                new TextWidget(x, 180, 200, 20, Text.translatable("conf.mhud.crosshair3dp_hud"), textRenderer)
+        );
+        addDrawableChild(
+                ButtonWidget.builder(
                 Mhud.CONFIG.Crosshair3dPerson ? ENABLED : DISABLED,
                 (widget) -> {
                     Mhud.CONFIG.Crosshair3dPerson = !Mhud.CONFIG.Crosshair3dPerson;
                     widget.setMessage(Mhud.CONFIG.Crosshair3dPerson ? ENABLED : DISABLED);
                     Mhud.CONFIG.save();
                 }
-        ).dimensions(x, 200, 200, 20).build();
+            ).dimensions(x, 200, 200, 20).build()
+        );
 
-        TextWidget crosshairInd_text = new TextWidget(x, 230, 200, 20, Text.translatable("conf.mhud.crosshairInd_hud"), textRenderer);
-        ButtonWidget crosshairInd_hud = ButtonWidget.builder(
+        addDrawableChild(
+                new TextWidget(x, 230, 200, 20, Text.translatable("conf.mhud.crosshairInd_hud"), textRenderer)
+        );
+        addDrawableChild(
+                ButtonWidget.builder(
                 Mhud.CONFIG.CrosshairIndicator ? ENABLED : DISABLED,
                 (widget) -> {
                     Mhud.CONFIG.CrosshairIndicator = !Mhud.CONFIG.CrosshairIndicator;
                     widget.setMessage(Mhud.CONFIG.CrosshairIndicator ? ENABLED : DISABLED);
                     Mhud.CONFIG.save();
                 }
-        ).dimensions(x, 250, 200, 20).build();
+            ).dimensions(x, 250, 200, 20).build()
+        );
 
-        TextWidget small_utils_text = new TextWidget(x, 280, 200, 20, Text.translatable("conf.mhud.small_utils_hud"), textRenderer);
-        ButtonWidget small_utils_hud = ButtonWidget.builder(
+        addDrawableChild(
+                new TextWidget(x, 280, 200, 20, Text.translatable("conf.mhud.small_utils_hud"), textRenderer)
+        );
+        addDrawableChild(
+                ButtonWidget.builder(
                 Mhud.CONFIG.SmallUtils ? ENABLED : DISABLED,
                 (widget) -> {
                     Mhud.CONFIG.SmallUtils = !Mhud.CONFIG.SmallUtils;
                     widget.setMessage(Mhud.CONFIG.SmallUtils ? ENABLED : DISABLED);
                     Mhud.CONFIG.save();
                 }
-        ).dimensions(x, 300, 200, 20).build();
+            ).dimensions(x, 300, 200, 20).build()
+        );
 
-        TextWidget side_shield_text = new TextWidget(x, 330, 200, 20, Text.translatable("conf.mhud.side_shield_hud"), textRenderer);
-        ButtonWidget side_shield_hud = ButtonWidget.builder(
+        addDrawableChild(
+                new TextWidget(x, 330, 200, 20, Text.translatable("conf.mhud.side_shield_hud"), textRenderer)
+        );
+        addDrawableChild(
+                ButtonWidget.builder(
                 Mhud.CONFIG.SideShield ? ENABLED : DISABLED,
                 (widget) -> {
                     Mhud.CONFIG.SideShield = !Mhud.CONFIG.SideShield;
                     widget.setMessage(Mhud.CONFIG.SideShield ? ENABLED : DISABLED);
                     Mhud.CONFIG.save();
                 }
-        ).dimensions(x, 350, 200, 20).build();
+            ).dimensions(x, 350, 200, 20).build()
+        );
 
-        TextWidget nameplate3d_text = new TextWidget(x, 380, 200, 20, Text.translatable("conf.mhud.nameplate_3dp_hud"), textRenderer);
-        ButtonWidget nameplate3d_hud = ButtonWidget.builder(
+        addDrawableChild(
+                new TextWidget(x, 380, 200, 20, Text.translatable("conf.mhud.nameplate_3dp_hud"), textRenderer)
+        );
+        addDrawableChild(
+            ButtonWidget.builder(
                 Mhud.CONFIG.Nameplate3dPerson ? ENABLED : DISABLED,
                 (widget) -> {
                     Mhud.CONFIG.Nameplate3dPerson = !Mhud.CONFIG.Nameplate3dPerson;
                     widget.setMessage(Mhud.CONFIG.Nameplate3dPerson ? ENABLED : DISABLED);
                     Mhud.CONFIG.save();
                 }
-        ).dimensions(x, 400, 200, 20).build();
+            ).dimensions(x, 400, 200, 20).build()
+        );
 
-        ButtonWidget close = ButtonWidget.builder(
+        addDrawableChild(ButtonWidget.builder(
                 Text.translatable("conf.mhud.close"),
                 (widget) -> {
                     MinecraftClient.getInstance().setScreen(null);
                 }
-        ).dimensions(x, 430, 200, 20).build();
-
-        addDrawableChild(armor_text);
-        addDrawableChild(potion_text);
-        addDrawableChild(crosshair3d_text);
-        addDrawableChild(crosshairInd_text);
-        addDrawableChild(crosshair3d_hud);
-        addDrawableChild(crosshairInd_hud);
-        addDrawableChild(side_shield_hud);
-        addDrawableChild(side_shield_text);
-        addDrawableChild(small_utils_hud);
-        addDrawableChild(nameplate3d_hud);
-        addDrawableChild(nameplate3d_text);
-        addDrawableChild(small_utils_text);
-        addDrawableChild(armor_hud);
-        addDrawableChild(potion_hud);
-        addDrawableChild(close);
+            ).dimensions(x, 430, 200, 20).build()
+        );
     }
 }
